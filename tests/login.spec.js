fa99c0d25e0bd533162716fdd7625ea6bb71a383
varsha1990 <varsha.bagewadi@gmail.com>
2026-07-11 10:49:14 +0530

    Initial commit

---
+++ tests/login.spec.js
@@ -0,0 +1,10 @@
+import {test} from "@playwright/test"
+
+test("login",async({page})=>{
+
+    await page.goto("https://www.saucedemo.com/")
+    await page.locator("#user-name").fill("standard_user")
+    await page.locator("#password").fill("secret_sauce")
+    await page.locator("#login-button").click()
+
+})
